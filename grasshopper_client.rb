53e28886c5131697d31149be4c955f5d3f807e06
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2011-01-08 17:43:35 +0100

    initial version of grasshopper client

---
+++ grasshopper_client.rb
@@ -0,0 +1,81 @@
+require "rubygems"
+require 'yajl'
+require 'yajl/http_stream'
+
+require "pp"
+
+require "httparty"
+
+# Yajl::HttpStream.get(URI.parse("http://gambit.local:3000/world")) do |frame|
+#   pp frame
+# end
+
+
+class Grasshopper
+  include HTTParty
+  
+  attr_accessor :world, :server
+
+  def self.join(server, nick, &block)
+    game = new(server, nick)
+    Yajl::HttpStream.get(URI.parse("http://#{server}/world")) do |world|
+      game.world = world
+      game.instance_eval(&block)
+      game.update_player
+    end
+  end
+
+  def initialize(server, nick)
+    @player = {:nick => nick, :actions => {:thrust => false, :turn_left => false, :turn_right => false, :fire => false}}
+    @server = server
+    self.class.post("http://#{@server}/players/#{@player[:nick]}")
+  end
+
+  def run!
+    @player[:actions][:thrust] = true
+  end
+  def stop!
+    @player[:actions][:thrust] = false
+  end
+  def turn_left!
+    @player[:actions][:turn_left] = true
+    @player[:actions][:turn_right] = false
+  end
+  def turn_right!
+    @player[:actions][:turn_left] = false
+    @player[:actions][:turn_right] = true
+  end
+  def stop_turning!
+    @player[:actions][:turn_left] = false
+    @player[:actions][:turn_right] = false
+  end
+  def shoot!
+    @player[:actions][:shoot] = true
+  end
+  def cease_fire!
+    @player[:actions][:shoot] = false
+  end
+  
+  def update_player
+    puts "Sending #{@player[:actions].inspect} to http://#{@server}/players/#{@player[:nick]}"
+    self.class.put("http://#{@server}/players/#{@player[:nick]}", :body => Yajl::Encoder.encode(@player))
+  end
+end
+
+
+
+# require "grashopper"
+
+Grasshopper.join("gambit.local:3000", "andi") do
+  world
+  
+  run!
+  # stop!
+  # 
+  # turn_left!
+  # turn_right!
+  # stop_turning!
+  # 
+  # shoot!
+  # cease_fire!
+end
