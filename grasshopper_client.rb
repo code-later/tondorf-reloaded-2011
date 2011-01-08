d43a2070103b7cc1b64876d38910906cd13c86e4
Andi Bade <andi@galaxycats.com>
2011-01-08 21:53:32 +0100

    refactored grasshopper client for testing purpose

--- grasshopper_client.rb
+++ grasshopper_client.rb
@@ -12,44 +12,59 @@ class Grasshopper
   
   attr_accessor :world, :server
 
-  def self.join(server, nick, &block)
+  def self.join(server, nick)
     game = new(server, nick)
-    Yajl::HttpStream.get(URI.parse("http://#{server}/world")) do |world|
-      game.world = world
-      game.instance_eval(&block)
-      game.update_player
+    if block_given?
+      Yajl::HttpStream.get(URI.parse("http://#{server}/world")) do |world|
+        game.world = world
+        game.instance_eval(yield)
+        game.update_player
+      end
     end
+    game
   end
 
   def initialize(server, nick)
     @player = {:nick => nick, :actions => {:thrust => false, :turnLeft => false, :turnRight => false, :fire => false}}
     @server = server
-    self.class.post("http://#{@server}/players/#{@player[:nick]}")
+    begin
+      self.class.post("http://#{@server}/players/#{@player[:nick]}")
+    rescue => e
+      puts "Could not connect to game-server"
+      raise e
+    end
   end
 
-  def run!
+  def run
     @player[:actions][:thrust] = true
   end
-  def stop!
+  def stop
     @player[:actions][:thrust] = false
   end
-  def turn_left!
+  def turn_left
     @player[:actions][:turn_left] = true
     @player[:actions][:turn_right] = false
   end
-  def turn_right!
+  def turn_right
     @player[:actions][:turn_left] = false
     @player[:actions][:turn_right] = true
   end
-  def stop_turning!
+  def stop_turning
     @player[:actions][:turn_left] = false
     @player[:actions][:turn_right] = false
   end
-  def shoot!
+  def shoot
     @player[:actions][:shoot] = true
   end
-  def cease_fire!
+  def cease_fire
     @player[:actions][:shoot] = false
+  end
+
+  %w(run stop turn_left turn_right stop_turning shoot cease_fire).each do |meth_name|
+    define_method "#{meth_name}!" do
+      send(meth_name)
+      update_player
+    end
   end
   
   def update_player
@@ -62,16 +77,21 @@ end
 
 # require "grashopper"
 
-Grasshopper.join("localhost:3000", "basti") do
-  world
-  
-  run!
-  # stop!
-  # 
-  turn_left!
-  # turn_right!
-  # stop_turning!
-  # 
-  # shoot!
-  # cease_fire!
-end
+# Grasshopper.join("localhost:3000", "andi1") do
+#   world
+#   
+#   run
+#   # stop!
+#   # 
+#   turn_left
+#   # turn_right!
+#   # stop_turning!
+#   # 
+#   # shoot!
+#   # cease_fire!
+# end
+
+# p = Grasshopper.join("localhost:3000", "andi2")
+# p.run!
+# p.stop!
+# p.turn_right!
